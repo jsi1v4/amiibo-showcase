864ec47069e8aa885122746e7c25f2e59864517c
jsi1v4 <git@jsi1v4.com>
2022-04-04 17:34:57 -0300

    feat: layouts added

---
+++ src/layouts/LayoutDefault.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { useAuthProvider } from '@/providers/auth';
+
+interface Props {
+  loading?: boolean;
+}
+
+const props = defineProps<Props>();
+
+const { user } = useAuthProvider();
+</script>
+
+<template>
+  <div class="layout-default">
+    <app-user :title="user?.fullName" />
+    <app-content>
+      <base-progress-bar v-show="props.loading" />
+      <slot />
+    </app-content>
+  </div>
+</template>
+
+<style lang="scss">
+.layout-default {
+  --app-bar-width: 0px;
+}
+</style>
