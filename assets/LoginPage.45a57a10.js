6dbd886cb87d0f994c082627ab7692ffea5e2474
jsi1v4 <git@jsi1v4.com>
2024-09-07 13:06:58 -0300

    Updates

--- assets/LoginPage.45a57a10.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./index.1aefb6c6.js";import{h as t,O as n,k as r,t as s}from"./vendor.8648d41f.js";const m=t({setup(a){const e=o();return n(()=>{e.login()}),(c,u)=>(r(),s("div"))}});export{m as default};
